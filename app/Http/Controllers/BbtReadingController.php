e68c1c9af91adef241ffa25d763c27bc8fca23d3
Rico23Y <ricoyarte23@gmail.com>
2026-04-05 16:17:31 +0800

    feat: add make_model

---
+++ app/Http/Controllers/BbtReadingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BbtReading;
+use App\Http\Requests\StoreBbtReadingRequest;
+use App\Http\Requests\UpdateBbtReadingRequest;
+
+class BbtReadingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBbtReadingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BbtReading $bbtReading)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BbtReading $bbtReading)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBbtReadingRequest $request, BbtReading $bbtReading)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BbtReading $bbtReading)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,15 @@ Route::inertia('/', 'welcome', [
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard/index')->name('dashboard');
+    Route::inertia('/dashboard', 'dashboard/index')->name('dashboard');
+    Route::inertia('calendar', 'calendar/index')->name('calendar');
+
+    Route::resource('cycles', CycleController::class);
+    Route::resource('bbt', BbtReadingController::class);
+    Route::resource('partners', PartnerController::class);
+    Route::resource('insights', InsightController::class);
+    
 });
 
 require __DIR__.'/settings.php';
+
