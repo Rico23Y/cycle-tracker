2f7f25e3d511edbc9476dbc04c47e512ca10b6e3
Rico23Y <ricoyarte23@gmail.com>
2026-06-24 14:44:44 +0800

    brand: update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any">
+        <link rel="alternate icon" href="/favicon.ico">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
